bbffcd761b78b32251256058e719c3984b725eab
AdityaDixit-2000 <adityadixitp123@gmail.com>
2024-11-18 17:20:00 +0530

    Final commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## In this project I created two main files of JS.
+## index.js and app.js :
+## index.js : In this file I write code for indentying is number is even or odd
+## app.js : In this file I write code for extracting all prime numbers from given array
+##      then perform some operations on that array.
+
+### Expeted outputs of codes are :
+#### primeArray = [ 19, 23, 13, 31, 29, 2, 11, 17, 5 ],
+#### minPrime = 2, maxPrime = 31 and sumPrimes = 150.
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const numbers = [
     19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
   ];
+  let primeArray = [];
   for(let i=0; i<numbers.length; i++){
     let flag = true;
     for(let j=2; j<numbers[i]; j++){
@@ -8,5 +9,25 @@ const numbers = [
             flag = false;break;
         }
     }
-    if(flag) console.log(numbers[i]);
+    if(flag) primeArray.push(numbers[i]);
   }
+//   console.log(primeArray);
+
+//   Finding Max and Min in primeArray
+let minPrime = Infinity;
+for(let k=0; k<primeArray.length; k++){
+    if(minPrime>primeArray[k]) minPrime = primeArray[k];
+}
+
+let maxPrime = -Infinity;
+for(let l=0; l<primeArray.length; l++){
+    if(maxPrime<primeArray[l]) maxPrime = primeArray[l];
+}
+// console.log(minPrime, maxPrime);
+
+//   Calculating sum of all numbers in primeArray
+let sumPrime = 0;
+for(let m=0; m<primeArray.length; m++){
+    sumPrime += primeArray[m];
+}
+// console.log(sumPrime);
